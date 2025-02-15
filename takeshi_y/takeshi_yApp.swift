5857b4f8a0bd07ee167c9edd3cc9e2a539b3032e
monta-marin <kururu.misuke@gmail.com>
2025-02-15 01:12:32 -0800

    Initial Commit

---
+++ takeshi_y/takeshi_yApp.swift
@@ -0,0 +1,17 @@
+//
+//  takeshi_yApp.swift
+//  takeshi_y
+//
+//  Created by 山崎猛 on 2025/02/15.
+//
+
+import SwiftUI
+
+@main
+struct takeshi_yApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
